fa89dd386b4388f87534c916d5f021aa264f8edd
AurelienB20 <134041681+AurelienB20@users.noreply.github.com>
2024-03-26 08:53:28 +0100

    Update relu6.c

--- c/relu6.c
+++ c/relu6.c
@@ -1,39 +1,36 @@
 #include <stdio.h>
+#include <math.h>
 #include "array.h"
 
-// Fonction ReLU6
-double relu6(double x) {
-    return (x > 6.0) ? 6.0 : (x > 0.0) ? x : 0.0;
-}
-
-// Fonction pour appliquer ReLU6 à tous les éléments d'un tensor
-void apply_relu6_to_tensor(double* input, int* input_shape, int input_dim) {
-    int size = 1;
-
-    for (int i = 0; i < input_dim; i++){
-        size *= input_shape[i];
+void relu6(double** input, int* input_shape, int input_dim) {
+    int totalsize = 1;
+    for (int i = 0; i < input_dim; i++) {
+        totalsize *= input_shape[i];
     }
 
-    for (int i = 0; i < size; i++) {
-        input[i] = relu6(input[i]);
+    for (int i = 0; i < totalsize; i++) {
+        if ((*input)[i] < 0) {
+            (*input)[i] = 0;
+        } else if ((*input)[i] > 6) {
+            (*input)[i] = 6;
+        }
     }
 }
 
 int main() {
-    // Exemple d'utilisation
-    double input[] = {-3.0, -1.0, 0.0, 2.5, 5.0, 7.0};
-    int input_shape[2] = {2, 3};
-    int input_dim = 2;
+    int dim[1] = {3};
+    double* input = initArray(dim, 1);
+    fillArray(input, dim, 1, 1);
 
-    printf("Tensor avant ReLU6 :\n");
-    printArray(input, input_shape, input_dim);
+    printf("Input: ");
+    printArray(input, dim, 1);
 
-    // Appliquer ReLU6 au tensor
-    apply_relu6_to_tensor(input, input_shape, input_dim);
+    printf("\n");
+    relu6(&input, dim, 1);
 
-    // Afficher le tensor après l'application de ReLU6
-    printf("Tensor après ReLU6 :\n");
-    printArray(input, input_shape, input_dim);
+    printf("ReLU6 output: ");
+    printArray(input, dim, 1);
+    printf("\n");
 
     return 0;
 }
