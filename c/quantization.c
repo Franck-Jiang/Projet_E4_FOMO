f05a301a4d1b0a7a607e75730d7d21e109ca8564
timotheedumas <133110240+timotheedumas@users.noreply.github.com>
2024-04-23 14:04:27 +0200

    Update quantization.c

--- c/quantization.c
+++ c/quantization.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h> // Pour uint8_t
-#include "array.h" // Inclure le fichier d'en-tête contenant les déclarations des fonctions
+#include <stdint.h> 
+#include "array.h" 
 
-void toUint8(double* input, uint8_t* output, int size) {
+void quantization(double* input, uint8_t* output, int size, double factor) {
     for (int i = 0; i < size; i++) {
-        double value = input[i];
-        // Arrondir la valeur à l'entier le plus proche
-        if (value < 0) value = 0; // Assurer que la valeur n'est pas négative
-        if (value > 255) value = 255; // Assurer que la valeur ne dépasse pas 255
-        output[i] = (uint8_t)(value + 0.5); // Arrondi au plus proche entier
+        double value = factor * (input[i] + 128); 
     }
 }
 
@@ -21,17 +17,16 @@ int main() {
     printf("Input: ");
     printArray(input, dim, 1);
 
-    // Appel de la fonction toUint8
-    uint8_t* output = malloc(3 * sizeof(uint8_t)); // Allouer de la mémoire pour le tableau de sortie
-    toUint8(input, output, 3); // Appeler la fonction de quantification
+    // Appel de la fonction quantization avec le facteur spécifié
+    uint8_t* output = malloc(3 * sizeof(uint8_t)); 
+    quantization(input, output, 3, 0.00390625); 
 
-    printf("Output after quantification: ");
+    printf("Output after quantization: ");
     for (int i = 0; i < 3; i++) {
         printf("%d ", output[i]);
     }
     printf("\n");
 
-    // Libérer la mémoire allouée pour le tableau de sortie
-    free(output);
+    free(output); // Libérer la mémoire allouée pour le tableau de sortie
 
     return 0;
